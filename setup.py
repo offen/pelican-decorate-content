cff6b3dd5a6296cee40018734ee4d3f4fece6da6
Frederik Ring <frederik.ring@gmail.com>
2020-08-02 19:08:46 +0200

    fix bad dependency declarations

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pelican_decorate_content",
-    version="0.1.0",
+    version="0.1.1",
     author="Frederik Ring",
     author_email="frederik.ring@gmail.com",
     description="A Pelican plugin for rule-based addition of classes to your content",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/offen/pelican-decorate-content",
     packages=["pelican_decorate_content"],
-    install_requires=["pelican>=3.0", "beautifulsoup4~=4.0.0"],
+    install_requires=["pelican>=3", "beautifulsoup4"],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
